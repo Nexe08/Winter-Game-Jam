a07d56c2449416c5d8f5f04081b625432b053784
Nexe <rohitrautela1408@gmail.com>
2021-12-27 15:21:17 +0530

    Implemented Santa animation

--- Script/Character/Senta.gd
+++ Script/Character/Senta.gd
@@ -6,9 +6,13 @@ var direction: Vector2
 var velocity: Vector2
 const UP = Vector2.UP
 
+onready var fsm = $AnimationTree.get('parameters/playback')
+onready var sprite = $Sprite
+
 func _physics_process(delta: float) -> void:
     velocity = move_and_slide(velocity, UP)
     _handel_movement(delta)
+    _handel_animation()
 
 func _handel_movement(delta):
     direction.x = int(Input.get_action_strength("d") - Input.get_action_strength("a"))
@@ -16,3 +20,15 @@ func _handel_movement(delta):
     
     velocity.x = lerp(velocity.x, movement_speed * direction.x, 5 * delta)
     velocity.y = lerp(velocity.y, movement_speed * direction.y, 5 * delta)
+
+func _handel_animation():
+    
+    if direction.x == 0 and direction.y == 0:
+        fsm.travel("idle")
+    else:
+        fsm.travel("walk")
+        
+        if velocity.x < 0:
+            sprite.flip_h = true
+        else:
+            sprite.flip_h = false
