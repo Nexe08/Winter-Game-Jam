2e830066b864192e4388f07f196cfd6cb6a97d1f
Nexe <rohitrautela1408@gmail.com>
2021-12-27 16:40:51 +0530

    Implemantead random map generation.

---
+++ Script/Main.gd
@@ -0,0 +1,39 @@
+extends Node2D
+# Main
+
+export (NodePath) var tile_map
+
+onready var map: TileMap = get_node(tile_map)
+var tile_size = 16
+var map_size = Vector2(1024, 600) / Vector2(tile_size, tile_size)
+
+
+func _ready() -> void:
+    randomize()
+    _generate_map()
+
+func _generate_map():
+    _generte_border_propes()
+    _generte_inner_propes()
+
+func _generte_border_propes():
+    for i in [0, map_size.x]:
+        for j in range(0, map_size.y):
+            var ids = map.tile_set.get_tiles_ids()
+            ids.shuffle()
+            map.set_cell(i, j, ids.front())
+
+    for i in range(0, map_size.x):
+        for j in [0, map_size.y]:
+            var ids = map.tile_set.get_tiles_ids()
+            ids.shuffle()
+            map.set_cell(i, j, ids.front())
+
+func _generte_inner_propes():
+    for i in range(1, map_size.x - 1):
+        for j in range(1, map_size.y - 1):
+            var chance = randi() % 100
+            if chance > 45 and chance < 55:
+                var ids = map.tile_set.get_tiles_ids()
+                ids.shuffle()
+                map.set_cell(i, j, ids.front())
