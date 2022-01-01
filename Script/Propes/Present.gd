08f61c2ab29abcb656570ab0ed7784f6dbced7a8
Nexe <rohitrautela1408@gmail.com>
2022-01-01 15:26:56 +0530

    Added partical for picking gift.

--- Script/Propes/Present.gd
+++ Script/Propes/Present.gd
@@ -10,17 +10,24 @@ var texture_choice = [
 func _ready() -> void:
     _choose_random_position()
     texture_choice.shuffle()
-    $Sprite.texture = load(texture_choice.front())
+    _handel_sprite_property()
 
 func _choose_random_position():
+    _handel_sprite_property()
     var ss = get_viewport_rect().size
     # 16 is tile size
     var choosen_position = Vector2(rand_range(16, (ss.x - 16)), rand_range(16, (ss.y - 16)))
     global_position = choosen_position
 
+func _handel_sprite_property():
+    $Sprite.texture = load(texture_choice.front())
+    $Sprite.visible = true
+
 func pick_up(node):
     # disapear animation
     if node.has_method("_gift_picked_up"):
         node._gift_picked_up(1)
-    _choose_random_position()
+    
+    $AnimationPlayer.play("picked_up")
+#    _choose_random_position()
     # apear animation
