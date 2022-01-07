72d48bd5a58a83b6a46c569c01f2d310cd1ba212
Nexe <rohitrautela1408@gmail.com>
2022-01-07 10:10:55 +0530

    Senta can pick hold only one gift

--- Script/PresentDetector.gd
+++ Script/PresentDetector.gd
@@ -7,6 +7,7 @@ onready var parent = get_parent() # senta
 
 
 func _on_PresentDetector_area_entered(area: Area2D) -> void:
-    if area.has_method("pick_up"):
-        area.pick_up(self)
-        inventory.add_gift_in_inventory(1)
+    if inventory.can_pick:
+        if area.has_method("pick_up"):
+            area.pick_up(self)
+            inventory.add_gift_in_inventory(1)
