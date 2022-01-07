1ab641608cb395ceb1b5a47f72f5c3acaf18de99
Nexe <rohitrautela1408@gmail.com>
2022-01-07 14:52:42 +0530

    Child spawn at random position at start of game

--- Script/Character/Child.gd
+++ Script/Character/Child.gd
@@ -1,3 +1,11 @@
 extends KinematicBody2D
 # Child
 
+func _ready() -> void:
+    _choose_random_position()
+
+func _choose_random_position():
+    var ss = get_viewport_rect().size
+    # 16 is tile size
+    var choosen_position = Vector2(rand_range(16, (ss.x - 16)), rand_range(16, (ss.y - 16)))
+    global_position = choosen_position
