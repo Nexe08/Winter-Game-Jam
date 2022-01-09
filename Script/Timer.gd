f911c500fd40bd6a812c2dcf6d401939d317b5fd
Nexe <rohitrautela1408@gmail.com>
2022-01-09 17:00:37 +0530

    Implemented: Game over after time out.

--- Script/Timer.gd
+++ Script/Timer.gd
@@ -6,3 +6,7 @@ var time = 120
 func _process(delta: float) -> void:
     time -= delta
     text = String(int(time))
+    
+    if time <= 0:
+        Global.emit_signal("game_over")
+        queue_free()
