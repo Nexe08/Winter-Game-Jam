3f03a34fa7068faaefcd66856957c8f6ad1bd0e2
Nexe <rohitrautela1408@gmail.com>
2022-01-10 10:20:03 +0530

    Added link button for twitter handel and music creator.

--- Script/MainMenu.gd
+++ Script/MainMenu.gd
@@ -4,3 +4,10 @@ extends Control
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_released("space"):
         Global.emit_signal("start_game")
+
+
+func _on_LinkButton_pressed() -> void:
+    OS.shell_open("https://pixabay.com/music/christmas-jingle-bells-11295/")
+
+func _on_LinkButton2_pressed() -> void:
+    OS.shell_open("https://twitter.com/nexe0044")
